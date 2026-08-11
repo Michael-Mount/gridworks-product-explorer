71731618323f58d799153ba79a85590d850d85b5
Michael Mount <michaeljoesphmount@gmail.com>
2026-08-11 01:07:07 -0400

    refactored productexplore search to search for name.model, and description for wider range of search

--- src/components/ProductExplorer/ProductExplorer.tsx
+++ src/components/ProductExplorer/ProductExplorer.tsx
@@ -10,13 +10,47 @@ interface ProductExplorerProps {
 
 export default function ProductExplorer({ products }: ProductExplorerProps) {
   const [showFeaturedProducts, setShowFeaturedProducts] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const featuredProducts = products.filter((product) => product.featured);
+  const normalizedSearchTerm = searchTerm.toLowerCase();
 
-  const visibleProducts = showFeaturedProducts ? featuredProducts : products;
+  //Search Terms
+  const searchedProducts = products.filter((product) => {
+    const matchesName = product.name
+      .toLowerCase()
+      .includes(normalizedSearchTerm);
+
+    const matchesModel = product.model
+      .toLowerCase()
+      .includes(normalizedSearchTerm);
+
+    const matchesDescripton = product.description
+      .toLowerCase()
+      .includes(normalizedSearchTerm);
+
+    return matchesName || matchesModel || matchesDescripton;
+  });
+
+  //Create a list of featured products from the search
+  const featuredProducts = searchedProducts.filter(
+    (product) => product.featured,
+  );
+
+  //decide wether the search displays or the displayed products from the search display
+  const visibleProducts = showFeaturedProducts
+    ? featuredProducts
+    : searchedProducts;
 
   return (
     <div>
+      <label>
+        Search Products
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          type="text"
+        />
+      </label>
       <button
         onClick={() => {
           setShowFeaturedProducts(
