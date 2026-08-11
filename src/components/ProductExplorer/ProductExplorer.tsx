26fa196c7e192c32a446176e325cb8c6b395486f
Michael Mount <michaeljoesphmount@gmail.com>
2026-08-11 02:40:01 -0400

    productexplorer updated to have dropdown to create more filtering, needs to be connected to the pipeline

--- src/components/ProductExplorer/ProductExplorer.tsx
+++ src/components/ProductExplorer/ProductExplorer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import type { Product } from "@/types/product";
+import type { Product, ProductCategory } from "@/types/product";
 import ProductGrid from "../ProductGrid/ProductGrid";
 
 interface ProductExplorerProps {
@@ -11,6 +11,9 @@ interface ProductExplorerProps {
 export default function ProductExplorer({ products }: ProductExplorerProps) {
   const [showFeaturedProducts, setShowFeaturedProducts] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState<
+    ProductCategory | "all"
+  >("all");
 
   const normalizedSearchTerm = searchTerm.toLowerCase().trim();
 
@@ -51,6 +54,25 @@ export default function ProductExplorer({ products }: ProductExplorerProps) {
           type="text"
         />
       </label>
+      <label>
+        Category:
+        <select
+          value={selectedCategory}
+          onChange={(e) => {
+            const newCategory = e.target.value as ProductCategory | "all";
+            setSelectedCategory(newCategory);
+          }}
+        >
+          <option value="all">All</option>
+          <option value="protection">Protection</option>
+          <option value="automation">Automation</option>
+          <option value="networking">Networking</option>
+          <option value="monitoring">Monitoring</option>
+          <option value="communications">Communications</option>
+          <option value="switching">Switching</option>
+        </select>
+      </label>
+
       <button
         aria-pressed={showFeaturedProducts}
         onClick={() => {
